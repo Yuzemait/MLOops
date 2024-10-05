11fff90ea4c30d892183c128670bb08cd02deae5
Yuzemait <jose.cota@iteso.mx>
2024-10-05 08:14:01 -0600

    Tests corrected

--- hypothesis.py
+++ hypothesis.py
@@ -1,18 +1,18 @@
 from scipy.stats import chisquare, ks_2samp
 import pandas as pd
-from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
 
 
 # Load datasets
-expected_df = pd.read_csv('./data/credit_train.csv')
-observed_df = pd.read_csv('./data/credit_pred_with_predictions.csv')
+df = pd.read_csv('./data/new_data.csv')
+
+train_df, test_df = train_test_split(df, test_size=0.3, random_state=42)
 
 # Perform Chi-squared test
 
-observed_freq = observed_df['Y'].value_counts(normalize=True)
-expected_freq = expected_df['Y'].value_counts(normalize=True)  
+observed_freq = test_df['Y'].value_counts(normalize=True)
+expected_freq = train_df['Y'].value_counts(normalize=True)
 
-print(observed_freq, expected_freq)
 
 chi_stat, p_val_chi = chisquare(observed_freq, expected_freq)
 print(f"Chi Stat: {chi_stat}    P-Val: {p_val_chi}")
@@ -22,7 +22,7 @@ else:
     print("Chi-squared test passed")
 
 # Apply Kolmogorov-Smirnov test
-ks_stat, p_val_ks = ks_2samp(observed_df['Y'], expected_df['Y'])
+ks_stat, p_val_ks = ks_2samp(train_df['Y'], test_df['Y'])
 print(f"KS Stat: {ks_stat}    P-Val: {p_val_ks}")
 if p_val_ks < 0.05:
     print("Kolmogorov-Smirnov test failed: Distributions are significantly different.")
